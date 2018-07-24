38087622f1981fbc013637a4790dfddbfe42aacf
DiegoB <dsb4k8@gmail.com>
2018-07-24 14:31:41 -0500

    Accuracy Metric: ~ 15%
    Added pre-trained classifier to increase accuracy of predictions. This is a lengthy process. During Execution, Node 21 takes a while to pass... Standing by for completion.

--- gendata.py
+++ gendata.py
@@ -1,8 +1,9 @@
 import pandas as pd 
-import numpy as np 
+import numpy as np
 import cPickle #for unzipping of data
 #for data conversion
-import
+import graphlab as gl 
+
 
 def unpickle(file):
 	fo = open(file, "rb")
@@ -29,3 +30,28 @@ test = get_dataframe(batch_test)
 
 print train.head()
 print train.shape, test.shape
+
+gltrain = gl.SFrame(train)
+gltest = gl.SFrame(test)
+model = gl.neuralnet_classifier.create(gltrain, target = "label", validation_set = None)
+model.evaluate(gltest)
+
+
+gltrain['glimage'] = gl.SArray(gltrain['image']).pixel_array_to_image(32,32,3, allow_rounding = True)
+gltest['glimage'] = gl.SArray(gltest['image']).pixel_array_to_image(32,32,3, allow_rounding = True)
+
+gltrain.remove_column("image")
+gltest.remove_column("image")
+gltrain.head()
+
+gltrain['image'] = gl.image_analysis.resize(gltrain['glimage'], 256,256,3)
+gltest['image'] = gl.image_analysis.resize(gltest['glimage'], 256,256,3)
+
+gltrain.remove_column('glimage')
+gltest.remove_column('glimage')
+gltrain.head()
+
+pretrained_model = gl.load_model('http://s3.amazonaws.com/GraphLab-Datasets/deeplearning/imagenet_model_iter45')
+gltrain['features'] = pretrained_model.extract_features(gltrain)
+gltest['features'] = pretrained_model.extract_features(gltest)
+gltrain.head()
